66f02286b9a48a05170d6aa30ad2c2bd62ab2286
João Paulo Freitas Pinho <joaopaulofreitasp23@gmail.com>
2026-04-30 19:52:30 -0300

    feat: create database class and integrate with route handler

---
+++ src/database/database.js
@@ -0,0 +1,21 @@
+import fs from "node:fs/promises"
+
+const DATABASE_PATH = new URL("db.json", import.meta.url)
+
+export class Database {
+  #database = {};
+
+  constructor() {
+    fs.readFile(DATABASE_PATH, "utf-8")
+      .then((data) => {
+        this.#database = JSON.parse(data); // se o arquivo existir, transforma o conteúdo em um objeto e armazena na propriedade #database
+      })
+      .catch(() => {
+        this.#persist(); // se o arquivo não existir, cria um novo com um objeto vazio
+      })
+  }
+
+  #persist() {
+    fs.writeFile(DATABASE_PATH, JSON.stringify(this.#database));
+  }
+}
